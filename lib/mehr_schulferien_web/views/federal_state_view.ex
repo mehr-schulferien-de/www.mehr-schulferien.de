8495af27ffbcc0e4e242450dc9855607bfb7d457
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2017-10-23 22:53:15 +0200

    Adds Event Schema.

--- lib/mehr_schulferien_web/views/federal_state_view.ex
+++ lib/mehr_schulferien_web/views/federal_state_view.ex
@@ -2,4 +2,5 @@ defmodule MehrSchulferienWeb.FederalStateView do
   use MehrSchulferienWeb, :view
   alias MehrSchulferienWeb.Formatter
   alias MehrSchulferienWeb.CalendarHelper
+  alias MehrSchulferienWeb.SchemaHelper
 end
