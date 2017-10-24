7f5420a208763382474ba3de64709e9cb569b73e
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2017-10-24 09:37:48 +0200

    Adds inverse calendar for index page.

--- lib/mehr_schulferien_web/controllers/page_controller.ex
+++ lib/mehr_schulferien_web/controllers/page_controller.ex
@@ -6,14 +6,23 @@ defmodule MehrSchulferienWeb.PageController do
   alias MehrSchulferien.Locations.Country
   alias MehrSchulferien.Timetables.Category
   alias MehrSchulferien.Repo
+  alias MehrSchulferien.CollectData
   import Ecto.Query, only: [from: 2]
 
   def index(conn, _params) do
     {country, federal_states} = get_locations("deutschland")
+    {starts_on, ends_on} = get_dates()
+    categories = get_categories()
+    religion_categories = get_religion_categories()
+
+    days = CollectData.list_days([country] ++ federal_states,
+                                 starts_on: starts_on, ends_on: ends_on)
 
     render(conn, "index.html", federal_states: federal_states,
                                country: country,
-                               religion_categories: get_religion_categories())
+                               days: days,
+                               categories: categories,
+                               religion_categories: religion_categories)
   end
 
   defp get_locations(id) do
@@ -25,6 +34,31 @@ defmodule MehrSchulferienWeb.PageController do
     federal_states = Repo.all(query)
 
     {country, federal_states}
+  end
+
+  defp get_religion_categories() do
+    query = from c in Category, where: c.for_students == true and
+                                       c.is_a_religion == true
+    Repo.all(query)
+  end
+
+  defp get_dates(starts_on \\ nil, ends_on \\ nil) do
+    case {starts_on, ends_on} do
+      {nil, nil} -> {:ok, starts_on} = Date.from_erl({Date.utc_today.year, Date.utc_today.month, 1})
+                    ends_on = Date.add(starts_on, 364)
+      {starts_on, ends_on} -> {:ok, starts_on} = Ecto.Date.cast(starts_on)
+                              {:ok, ends_on} = Ecto.Date.cast(ends_on)
+                              {:ok, starts_on} = Date.from_erl(Ecto.Date.to_erl(starts_on))
+                              {:ok, ends_on} = Date.from_erl(Ecto.Date.to_erl(ends_on))
+    end
+
+    CollectData.ramp_up_to_full_months(starts_on, ends_on)
+  end
+
+  defp get_categories() do
+    query = from c in Category, where: c.for_students == true and
+                                       "Wochenende" != c.name
+    Repo.all(query)
   end
 
   defp get_religion_categories() do
