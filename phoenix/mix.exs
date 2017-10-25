5ff8e55e49d48591cd5c698e1422607938960c67
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2017-10-25 19:15:45 +0200

    Adds Google Analytics code.

--- phoenix/mix.exs
+++ phoenix/mix.exs
@@ -4,7 +4,7 @@ defmodule MehrSchulferien.Mixfile do
   def project do
     [
       app: :mehr_schulferien,
-      version: "0.0.1",
+      version: "2.0.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
