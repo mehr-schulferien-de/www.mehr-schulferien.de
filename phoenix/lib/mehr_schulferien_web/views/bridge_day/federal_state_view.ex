779f3266f338ad6ac2d21afc4869fed8f67b5de1
komlanvi <doncredas@gmail.com>
2018-12-05 21:51:13 +0100

    bridge days landing page view

--- phoenix/lib/mehr_schulferien_web/views/bridge_day/federal_state_view.ex
+++ phoenix/lib/mehr_schulferien_web/views/bridge_day/federal_state_view.ex
@@ -3,4 +3,15 @@ defmodule MehrSchulferienWeb.BridgeDay.FederalStateView do
   alias MehrSchulferienWeb.Formatter
   alias MehrSchulferienWeb.CalendarHelper
   alias MehrSchulferienWeb.SchemaOrgHelper
+
+  def render("index.json", %{days: days}) do
+    render_many(days, __MODULE__, "show.json", as: :days)
+  end
+
+  def render("show.json", %{days: days}) do
+    %{
+      "slug" => days.slug,
+      "value" => days.value,
+    }
+    end
 end
