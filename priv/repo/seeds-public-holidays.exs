b2c0a56d3f16ac9ce2709a0aebf6a56cd7995f5b
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-01-29 17:04:14 +0100

    WIP

--- priv/repo/seeds-public-holidays.exs
+++ priv/repo/seeds-public-holidays.exs
@@ -10,7 +10,10 @@ defmodule M do
   alias MehrSchulferien.Calendars.HolidayOrVacationType
 
   def import_json(year) do
-    json_public_holidays = "priv/repo/seeds.d/" <> Integer.to_string(year) <> ".json" |> File.read!() |> Poison.decode!()
+    json_public_holidays =
+      ("priv/repo/seeds.d/" <> Integer.to_string(year) <> ".json")
+      |> File.read!()
+      |> Poison.decode!()
 
     query =
       from(l in Location,
@@ -21,61 +24,51 @@ defmodule M do
     federal_states = Repo.all(query)
 
     federal_states
-    |> Enum.each(fn federal_state -> 
-      Map.keys(json_public_holidays[federal_state.code]) 
-        |> Enum.each(fn json_public_holiday_name ->
-          json_date = json_public_holidays[federal_state.code][json_public_holiday_name]["datum"]
-          json_hinweis = json_public_holidays[federal_state.code][json_public_holiday_name]["hinweis"]
-          case json_hinweis do
-            "" ->
-              Calendars.create_holiday_or_vacation_type
-            _ ->
-          end
-          unless json_hinweis == "" do
-            IO.puts federal_state.name
+    |> Enum.each(fn federal_state ->
+      Map.keys(json_public_holidays[federal_state.code])
+      |> Enum.each(fn json_public_holiday_name ->
+        json_date = json_public_holidays[federal_state.code][json_public_holiday_name]["datum"]
 
-            IO.puts json_public_holiday_name
-            IO.puts json_date
-            IO.puts json_hinweis
-            IO.puts ""
-          end
-        end)
-      # IO.puts json_public_holidays[federal_state.code]["Neujahrstag"]["datum"]
+        json_hinweis =
+          json_public_holidays[federal_state.code][json_public_holiday_name]["hinweis"]
+
+        unless json_hinweis == "" do
+          IO.puts(federal_state.name)
+
+          IO.puts(json_public_holiday_name)
+          IO.puts(json_date)
+          IO.puts(json_hinweis)
+          IO.puts("")
+        end
+      end)
     end)
 
-    def first_or_create(public_holiday_type) do
-      query =
-        from(l in HolidayOrVacationType,
-          where: l.name == ^public_holiday_type,
-          where: l.country_location_id == 1,
-          limit: 1
-        )
+    # def first_or_create(public_holiday_type) do
+    #   query =
+    #     from(l in HolidayOrVacationType,
+    #       where: l.name == ^public_holiday_type,
+    #       where: l.country_location_id == 1,
+    #       limit: 1
+    #     )
+    #
+    # holiday_or_vacation_type =
+    #   case Repo.one(query) do
+    #     nil ->
+    #       {:ok, holiday_or_vacation_type} =
+    #         Calendars.create_holiday_or_vacation_type(%
+    #           name: public_holiday_type,
+    #           country_location_id: 1
+    #         )
 
-      holiday_or_vacation_type =
-        case Repo.one(query) do
-          nil ->
-            {:ok, holiday_or_vacation_type} =
-              Calendars.create_holiday_or_vacation_type(%
-                name: public_holiday_type,
-                country_location_id: 1
-              )
+    #       holiday_or_vacation_type
 
-            holiday_or_vacation_type
-
-          holiday_or_vacation_type ->
-            holiday_or_vacation_type
-        end
-      
-    end
-
-    # json_public_holidays
-    #   |> Enum.each(fn json_federal_state ->
-
-    #   end)
+    #     holiday_or_vacation_type ->
+    #       holiday_or_vacation_type
+    #   end
+    # end
   end
-
 end
 
-Enum.each [2023], fn year ->
+Enum.each([2023], fn year ->
   M.import_json(year)
-end 
+end)
