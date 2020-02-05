e00da326ded8f464342629b1bdae6fe53150e7c9
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-02-05 18:14:18 +0100

    Improves dates display.

--- lib/mehr_schulferien_web/views/view_helpers.ex
+++ lib/mehr_schulferien_web/views/view_helpers.ex
@@ -14,4 +14,24 @@ defmodule MehrSchulferienWeb.ViewHelpers do
       Integer.to_string(date.month) |> String.pad_leading(2, "0")
     }."
   end
+
+  def format_date_range(from_date, till_date) do
+    case {from_date, till_date} do
+      {x, x} ->
+        format_date(x)
+
+      {x, y} ->
+        format_date(x) <> " - " <> format_date(y)
+    end
+  end
+
+  def format_date_range(from_date, till_date, :short) do
+    case {from_date, till_date} do
+      {x, x} ->
+        format_date(x, :short)
+
+      {x, y} ->
+        format_date(x, :short) <> " - " <> format_date(y, :short)
+    end
+  end
 end
