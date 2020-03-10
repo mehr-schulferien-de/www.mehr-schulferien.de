8917cbe5f002bf51273d58841238f85e973ca279
David Whitlock <alovedalongthe@gmail.com>
2020-03-10 12:16:50 +0700

    Moved show_assigns and faq_assigns functions to a ControllerHelpers module

--- lib/mehr_schulferien_web/controllers/county_controller.ex
+++ lib/mehr_schulferien_web/controllers/county_controller.ex
@@ -1,7 +1,8 @@
 defmodule MehrSchulferienWeb.CountyController do
   use MehrSchulferienWeb, :controller
 
-  alias MehrSchulferien.{Calendars.DateHelpers, Periods, Locations}
+  alias MehrSchulferien.Locations
+  alias MehrSchulferienWeb.ControllerHelpers, as: CH
 
   def show(conn, %{
         "country_slug" => country_slug,
@@ -14,33 +15,11 @@ defmodule MehrSchulferienWeb.CountyController do
     location_ids = [country.id, federal_state.id, county.id]
     cities = Locations.list_cities(county)
     today = Date.utc_today()
-    current_year = today.year
-    next_12_months_periods = Periods.chunk_one_year_school_periods(location_ids, today)
 
-    {next_3_years_headers, next_3_years_periods} =
-      Periods.chunk_multi_year_school_periods(location_ids, current_year, 3)
+    assigns =
+      [cities: cities, country_slug: country_slug, location: county] ++
+        CH.show_period_data(location_ids, today)
 
-    public_periods = Periods.list_multi_year_all_public_periods(location_ids, current_year, 3)
-
-    public_periods =
-      Enum.filter(public_periods, &(&1.holiday_or_vacation_type.name != "Wochenende"))
-
-    days = DateHelpers.create_3_years(current_year)
-    months = DateHelpers.get_months_map()
-    next_three_years = Enum.join([current_year, current_year + 1, current_year + 2], ", ")
-
-    render(conn, "show.html",
-      cities: cities,
-      county: county,
-      country_slug: country_slug,
-      current_year: current_year,
-      days: days,
-      months: months,
-      next_12_months_periods: next_12_months_periods,
-      next_3_years_headers: next_3_years_headers,
-      next_3_years_periods: next_3_years_periods,
-      next_three_years: next_three_years,
-      public_periods: public_periods
-    )
+    render(conn, "show.html", assigns)
   end
 end
--- lib/mehr_schulferien_web/controllers/federal_state_controller.ex
+++ lib/mehr_schulferien_web/controllers/federal_state_controller.ex
@@ -1,7 +1,8 @@
 defmodule MehrSchulferienWeb.FederalStateController do
   use MehrSchulferienWeb, :controller
 
-  alias MehrSchulferien.{Calendars.DateHelpers, Periods, Locations}
+  alias MehrSchulferien.Locations
+  alias MehrSchulferienWeb.ControllerHelpers, as: CH
 
   @digits ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
@@ -19,78 +20,11 @@ defmodule MehrSchulferienWeb.FederalStateController do
     federal_state = Locations.get_federal_state_by_slug!(federal_state_slug, country)
     location_ids = [country.id, federal_state.id]
     today = Date.utc_today()
-    current_year = today.year
 
     assigns =
-      [current_year: current_year, location: federal_state] ++
-        show_assigns(location_ids, today, current_year) ++
-        dates_assigns(current_year) ++ faq_assigns(location_ids, today)
+      [location: federal_state] ++
+        CH.show_period_data(location_ids, today) ++ CH.faq_data(location_ids, today)
 
     render(conn, "show.html", assigns)
-  end
-
-  defp show_assigns(location_ids, today, current_year) do
-    next_12_months_periods = Periods.chunk_one_year_school_periods(location_ids, today)
-
-    {next_3_years_headers, next_3_years_periods} =
-      Periods.chunk_multi_year_school_periods(location_ids, current_year, 3)
-
-    public_periods = Periods.list_multi_year_all_public_periods(location_ids, current_year, 3)
-
-    public_periods =
-      Enum.filter(public_periods, &(&1.holiday_or_vacation_type.name != "Wochenende"))
-
-    [
-      next_12_months_periods: next_12_months_periods,
-      next_3_years_headers: next_3_years_headers,
-      next_3_years_periods: next_3_years_periods,
-      public_periods: public_periods
-    ]
-  end
-
-  defp dates_assigns(current_year) do
-    days = DateHelpers.create_3_years(current_year)
-    months = DateHelpers.get_months_map()
-
-    next_three_years =
-      MehrSchulferienWeb.ViewHelpers.comma_join_with_a_final_und([
-        "#{current_year}",
-        "#{current_year + 1}",
-        "#{current_year + 2}"
-      ])
-
-    [days: days, months: months, next_three_years: next_three_years]
-  end
-
-  defp faq_assigns(location_ids, today) do
-    todays_public_holiday_periods = Periods.list_public_holiday_periods(location_ids, today)
-
-    yesterdays_public_holiday_periods =
-      Periods.list_public_holiday_periods(location_ids, Date.add(today, -1))
-
-    tomorrows_public_holiday_periods =
-      Periods.list_public_holiday_periods(location_ids, Date.add(today, 1))
-
-    day_after_tomorrows_public_holiday_periods =
-      Periods.list_public_holiday_periods(location_ids, Date.add(today, 2))
-
-    todays_school_free_periods = Periods.list_school_free_periods(location_ids, today)
-
-    tomorrows_school_free_periods =
-      Periods.list_school_free_periods(location_ids, Date.add(today, 1))
-
-    day_after_tomorrows_school_free_periods =
-      Periods.list_school_free_periods(location_ids, Date.add(today, 2))
-
-    [
-      today: today,
-      todays_public_holiday_periods: todays_public_holiday_periods,
-      yesterdays_public_holiday_periods: yesterdays_public_holiday_periods,
-      tomorrows_public_holiday_periods: tomorrows_public_holiday_periods,
-      day_after_tomorrows_public_holiday_periods: day_after_tomorrows_public_holiday_periods,
-      todays_school_free_periods: todays_school_free_periods,
-      tomorrows_school_free_periods: tomorrows_school_free_periods,
-      day_after_tomorrows_school_free_periods: day_after_tomorrows_school_free_periods
-    ]
   end
 end
