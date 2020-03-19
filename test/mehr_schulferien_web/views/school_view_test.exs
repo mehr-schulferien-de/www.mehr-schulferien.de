fbe890ee8729027446a300d17e46523a2334d45e
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-19 20:18:46 +0100

    Add test to #truncate

---
+++ test/mehr_schulferien_web/views/school_view_test.exs
@@ -0,0 +1,29 @@
+defmodule MehrSchulferienWeb.SchoolViewTest do
+  use MehrSchulferienWeb.ConnCase, async: true
+
+  import MehrSchulferienWeb.SchoolView
+
+  describe "#truncate" do
+    test "it truncates a string with length > 28" do
+      string = "http://localhost:4000/ferien/d/schule/56077-grundschule-arenberg"
+
+      html_string =
+        {:safe,
+         [
+           "<abbr title=\"",
+           "http://localhost:4000/ferien/d/schule/56077-grundschule-arenberg",
+           "\">",
+           "http://localhost:4000/fer...",
+           "</abbr>\n"
+         ]}
+
+      assert truncate(string) == html_string
+    end
+
+    test "it doesn't truncate string of length < 28" do
+      string = "emmanuelhayford.com"
+
+      assert truncate(string) == "emmanuelhayford.com"
+    end
+  end
+end
