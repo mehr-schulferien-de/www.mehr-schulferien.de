71566a97f6a12407e815aabbdb85d181581dc469
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-19 18:51:37 +0100

    Display school url and email
    Fixes #189

--- lib/mehr_schulferien_web/views/school_view.ex
+++ lib/mehr_schulferien_web/views/school_view.ex
@@ -1,3 +1,18 @@
 defmodule MehrSchulferienWeb.SchoolView do
   use MehrSchulferienWeb, :view
+
+  def truncate(string) do
+    if String.length(string) > 28 do
+      ~E"""
+      <abbr title="<%= string %>"><%= snip(string) %></abbr>
+      """
+    else
+      string
+    end
+  end
+
+  defp snip(string) do
+    string
+    |> String.slice(0..27)
+  end
 end
