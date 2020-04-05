f002974c7563989b57172e4184b7debe76798d88
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-04-05 12:26:35 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MehrSchulferien.MixProject do
   def project do
     [
       app: :mehr_schulferien,
-      version: "3.1.7",
+      version: "3.1.8",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
