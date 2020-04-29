02f9ab3c52fd05bf8f527dce5657bfbb48fe1360
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-04-29 10:52:34 +0200

    Adds schema: "https" for the sitemap.xml

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MehrSchulferien.MixProject do
   def project do
     [
       app: :mehr_schulferien,
-      version: "3.2.30",
+      version: "3.2.31",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
