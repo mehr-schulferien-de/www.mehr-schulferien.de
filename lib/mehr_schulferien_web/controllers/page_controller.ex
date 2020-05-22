289426e9c0409ab9bac94f624e45b7659eb3f6a4
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-05-22 16:07:11 +0200

    Removes a/b test. closes #420

--- lib/mehr_schulferien_web/controllers/page_controller.ex
+++ lib/mehr_schulferien_web/controllers/page_controller.ex
@@ -11,41 +11,12 @@ defmodule MehrSchulferienWeb.PageController do
     months = DateHelpers.get_months_map()
     countries = Enum.map(Locations.list_countries(), &build_country_periods(&1, today, ends_on))
 
-    if Date.compare(today, ~D[2020-05-18]) == :lt do
-      # For an a/b test this creates a second smaller table which starts
-      # on the first next vacation date.
-      # TODO: Needs cleaning up after the a/b test.
-      today_b = ~D[2020-05-18]
-      ends_on_b = Date.add(today_b, 10)
-      [%{country: country, federal_states: federal_states}] = countries
-
-      periods_b =
-        Enum.reduce(federal_states, [], fn state, acc ->
-          acc ++
-            [
-              {state,
-               Periods.list_school_free_periods([state.id, country.id], today_b, ends_on_b)}
-            ]
-        end)
-
-      days_b = DateHelpers.create_days(today_b, 10)
-
-      render(conn, "index.html",
-        countries: countries,
-        days: days,
-        day_names: day_names,
-        months: months,
-        days_b: days_b,
-        periods_b: periods_b
-      )
-    else
-      render(conn, "index.html",
-        countries: countries,
-        days: days,
-        day_names: day_names,
-        months: months
-      )
-    end
+    render(conn, "index.html",
+      countries: countries,
+      days: days,
+      day_names: day_names,
+      months: months
+    )
   end
 
   def developers(conn, _params) do
