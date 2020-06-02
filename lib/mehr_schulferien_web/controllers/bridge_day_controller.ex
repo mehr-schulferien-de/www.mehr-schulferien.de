19e5fd89b8b30526af70d1f58a375812e901114d
David Whitlock <alovedalongthe@gmail.com>
2020-06-02 15:36:59 +0700

    Updated bridge days implementation for federal states

--- lib/mehr_schulferien_web/controllers/bridge_day_controller.ex
+++ lib/mehr_schulferien_web/controllers/bridge_day_controller.ex
@@ -3,7 +3,7 @@ defmodule MehrSchulferienWeb.BridgeDayController do
 
   alias MehrSchulferien.{Calendars.DateHelpers, Locations, Periods}
 
-  def show_within_federal_state(conn, %{
+  def index_within_federal_state(conn, %{
         "country_slug" => country_slug,
         "federal_state_slug" => federal_state_slug
       }) do
@@ -11,26 +11,54 @@ defmodule MehrSchulferienWeb.BridgeDayController do
     federal_state = Locations.get_federal_state_by_slug!(federal_state_slug, country)
     today = DateHelpers.today_berlin()
     current_year = today.year
-    {:ok, start_date} = Date.new(current_year, 1, 1)
-    {:ok, end_date} = Date.new(current_year, 12, 31)
+
+    render(conn, "index_within_federal_state.html",
+      country: country,
+      current_year: current_year,
+      federal_state: federal_state
+    )
+  end
+
+  def show_within_federal_state(conn, %{
+        "country_slug" => country_slug,
+        "federal_state_slug" => federal_state_slug,
+        "year" => year
+      }) do
+    year = check_year!(year)
+    country = Locations.get_country_by_slug!(country_slug)
+    federal_state = Locations.get_federal_state_by_slug!(federal_state_slug, country)
+    {:ok, start_date} = Date.new(year, 1, 1)
+    {:ok, end_date} = Date.new(year, 12, 31)
 
     public_periods =
       Periods.list_public_everybody_periods([country.id, federal_state.id], start_date, end_date)
 
     bridge_day_map = Periods.group_by_interval(public_periods)
-    days = DateHelpers.create_year(current_year)
+    days = DateHelpers.create_year(year)
     months = DateHelpers.get_months_map()
 
     assigns = [
       bridge_day_map: bridge_day_map,
       country: country,
-      current_year: current_year,
       days: days,
       federal_state: federal_state,
       months: months,
-      public_periods: public_periods
+      public_periods: public_periods,
+      year: year
     ]
 
     render(conn, "show_within_federal_state.html", assigns)
+  end
+
+  defp check_year!(year) do
+    year = String.to_integer(year)
+    today = DateHelpers.today_berlin()
+    current_year = today.year
+
+    if year in [current_year, current_year + 1, current_year + 2] do
+      year
+    else
+      raise MehrSchulferien.InvalidYearError
+    end
   end
 end
