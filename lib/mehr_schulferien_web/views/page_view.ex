1cf77eced97cc46a46e0c569204df09ad2ec8286
David Whitlock <alovedalongthe@gmail.com>
2020-06-13 13:13:06 +0700

    Fixes bugs in root page table

--- lib/mehr_schulferien_web/views/page_view.ex
+++ lib/mehr_schulferien_web/views/page_view.ex
@@ -1,30 +1,45 @@
 defmodule MehrSchulferienWeb.PageView do
   use MehrSchulferienWeb, :view
 
-  alias MehrSchulferien.{Calendars, Locations}
+  alias MehrSchulferien.Locations
 
   def number_schools() do
     Locations.number_schools()
   end
 
-  def get_days_period(date, periods) do
-    case Calendars.find_all_periods(periods, date) do
+  def get_school_period(periods) do
+    case Enum.filter(periods, & &1.is_school_vacation) do
       [] -> nil
       [period] -> period
-      periods -> periods |> Enum.sort(&(&1.display_priority >= &2.display_priority)) |> hd
+      periods -> select_display_period(periods)
     end
   end
+
+  defp select_display_period(periods) do
+    periods |> Enum.sort(&(&1.display_priority >= &2.display_priority)) |> hd
+  end
+
+  def display_period_info?(_, _, nil), do: false
 
   def display_period_info?(date, dates, period) do
-    if date == hd(dates) do
-      true
-    else
-      period.is_school_vacation == true && date == period.starts_on
+    if period.is_school_vacation do
+      if date == hd(dates) do
+        true
+      else
+        date == period.starts_on
+      end
     end
   end
 
-  def get_period_colspan(date, period) do
-    Date.diff(period.ends_on, date) + 1
+  def get_period_colspan(date, last_date, period) do
+    ends_on =
+      if Date.compare(last_date, period.ends_on) == :gt do
+        period.ends_on
+      else
+        last_date
+      end
+
+    Date.diff(ends_on, date) + 1
   end
 
   def show_period_info(period, colspan) do
@@ -45,5 +60,23 @@ defmodule MehrSchulferienWeb.PageView do
 
   defp show_period_date(period) do
     "(#{ViewHelpers.format_date_range(period.starts_on, period.ends_on, :short)})"
+  end
+
+  def get_non_school_period(_, nil, periods) do
+    case Enum.filter(periods, &non_school_period/1) do
+      [] -> nil
+      [period] -> period
+      periods -> select_display_period(periods)
+    end
+  end
+
+  def get_non_school_period(date, period, periods) do
+    if Date.compare(date, period.ends_on) == :gt do
+      get_non_school_period(date, nil, periods)
+    end
+  end
+
+  defp non_school_period(period) do
+    period.holiday_or_vacation_type.name == "Wochenende" or period.is_school_vacation == false
   end
 end
