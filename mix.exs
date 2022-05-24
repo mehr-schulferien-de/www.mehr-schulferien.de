6151089eebb929330a0836079b8529453a548408
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-05-24 14:20:25 +0200

    Adds a link from schools to federal_states.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MehrSchulferien.MixProject do
   def project do
     [
       app: :mehr_schulferien,
-      version: "3.4.2",
+      version: "3.4.3",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
