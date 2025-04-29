27b33f82932f35c6873c95bb4499409fcbf63987
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-04-29 08:04:15 +0200

    Remove ability to add or edit periods via web interface
    - Removed new_period and create_period actions from School, FederalState, and City controllers
    - Deleted period form templates
    - Cleaned up unused imports, aliases, and functions
    - All other period-related functionality remains intact

--- lib/mehr_schulferien_web/controllers/city_controller.ex
+++ lib/mehr_schulferien_web/controllers/city_controller.ex
@@ -1,75 +1,8 @@
 defmodule MehrSchulferienWeb.CityController do
   use MehrSchulferienWeb, :controller
 
-  import MehrSchulferienWeb.Authorize
-
-  alias MehrSchulferien.{Calendars, Calendars.DateHelpers, Locations}
-  alias MehrSchulferien.{Periods, Periods.Period}
+  alias MehrSchulferien.{Calendars.DateHelpers, Locations}
   alias MehrSchulferienWeb.ControllerHelpers, as: CH
-  alias MehrSchulferienWeb.Email
-
-  plug :user_check when action in [:new_period, :create_period]
-
-  def new_period(conn, %{"country_slug" => country_slug, "city_slug" => city_slug}) do
-    user = conn.assigns.current_user
-
-    %{country: country, federal_state: federal_state, city: city} =
-      Locations.show_city_to_country_map(country_slug, city_slug)
-
-    holiday_or_vacation_type =
-      Calendars.get_holiday_or_vacation_type_by_name!(
-        "Schulschließung wegen der COVID-19-Pandemie (Corona)"
-      )
-
-    changeset = Periods.change_period(%Period{})
-
-    render(conn, "new.html",
-      changeset: changeset,
-      city: city,
-      country: country,
-      federal_state: federal_state,
-      holiday_or_vacation_type_id: holiday_or_vacation_type.id,
-      user_email: user.email
-    )
-  end
-
-  def create_period(conn, %{
-        "country_slug" => country_slug,
-        "city_slug" => city_slug,
-        "period" => period_params
-      }) do
-    case Periods.create_period(period_params) do
-      {:ok, period} ->
-        Email.period_added_notification(period)
-
-        conn
-        |> put_flash(
-          :info,
-          "Die Daten zur Schulschließung wegen der COVID-19-Pandemie wurden eingetragen."
-        )
-        |> redirect(to: Routes.city_path(conn, :show, country_slug, city_slug))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        user = conn.assigns.current_user
-
-        %{country: country, federal_state: federal_state, city: city} =
-          Locations.show_city_to_country_map(country_slug, city_slug)
-
-        holiday_or_vacation_type =
-          Calendars.get_holiday_or_vacation_type_by_name!(
-            "Schulschließung wegen der COVID-19-Pandemie (Corona)"
-          )
-
-        render(conn, "new.html",
-          changeset: changeset,
-          city: city,
-          country: country,
-          federal_state: federal_state,
-          holiday_or_vacation_type_id: holiday_or_vacation_type.id,
-          user_email: user.email
-        )
-    end
-  end
 
   def show(conn, %{
         "country_slug" => country_slug,
--- lib/mehr_schulferien_web/controllers/federal_state_controller.ex
+++ lib/mehr_schulferien_web/controllers/federal_state_controller.ex
@@ -1,81 +1,11 @@
 defmodule MehrSchulferienWeb.FederalStateController do
   use MehrSchulferienWeb, :controller
 
-  import MehrSchulferienWeb.Authorize
-
   alias MehrSchulferien.{Calendars, Calendars.DateHelpers, Locations}
-  alias MehrSchulferien.{Periods, Periods.Period}
   alias MehrSchulferienWeb.ControllerHelpers, as: CH
-  alias MehrSchulferienWeb.Email
 
   @digits ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-  plug :user_check when action in [:new_period, :create_period]
-
-  def new_period(conn, %{
-        "country_slug" => country_slug,
-        "federal_state_slug" => federal_state_slug
-      }) do
-    user = conn.assigns.current_user
-    country = Locations.get_country_by_slug!(country_slug)
-    federal_state = Locations.get_federal_state_by_slug!(federal_state_slug, country)
-
-    holiday_or_vacation_type =
-      Calendars.get_holiday_or_vacation_type_by_name!(
-        "Schulschließung wegen der COVID-19-Pandemie (Corona)"
-      )
-
-    changeset = Periods.change_period(%Period{})
-
-    render(conn, "new.html",
-      changeset: changeset,
-      country: country,
-      federal_state: federal_state,
-      holiday_or_vacation_type_id: holiday_or_vacation_type.id,
-      user_email: user.email
-    )
-  end
-
-  def create_period(conn, %{
-        "country_slug" => country_slug,
-        "federal_state_slug" => federal_state_slug,
-        "period" => period_params
-      }) do
-    case Periods.create_period(period_params) do
-      {:ok, period} ->
-        Email.period_added_notification(period)
-
-        conn
-        |> put_flash(
-          :info,
-          "Die Daten zur Schulschließung wegen der COVID-19-Pandemie wurden eingetragen."
-        )
-        |> redirect(to: Routes.federal_state_path(conn, :show, country_slug, federal_state_slug))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        user = conn.assigns.current_user
-        country = Locations.get_country_by_slug!(country_slug)
-        federal_state = Locations.get_federal_state_by_slug!(federal_state_slug, country)
-        country = Locations.get_country_by_slug!(country_slug)
-
-        holiday_or_vacation_type =
-          Calendars.get_holiday_or_vacation_type_by_name!(
-            "Schulschließung wegen der COVID-19-Pandemie (Corona)"
-          )
-
-        render(conn, "new.html",
-          changeset: changeset,
-          country: country,
-          federal_state: federal_state,
-          holiday_or_vacation_type_id: holiday_or_vacation_type.id,
-          user_email: user.email
-        )
-    end
-  end
-
-  # Display all present and future dates for this holiday_or_vacation_type.
-  # If no future dates are available, then previous dates are displayed.
-  #
   def show_holiday_or_vacation_type(conn, %{
         "country_slug" => country_slug,
         "federal_state_slug" => federal_state_slug,
@@ -83,47 +13,21 @@ defmodule MehrSchulferienWeb.FederalStateController do
       }) do
     country = Locations.get_country_by_slug!(country_slug)
     federal_state = Locations.get_federal_state_by_slug!(federal_state_slug, country)
+    holiday_or_vacation_type = Calendars.get_holiday_or_vacation_type_by_slug!(holiday_or_vacation_type_slug)
 
     today = DateHelpers.today_berlin()
-    current_year = today.year
+    location_ids = [country.id, federal_state.id]
 
-    next_three_years =
-      MehrSchulferienWeb.ViewHelpers.comma_join_with_a_final_und([
-        "#{current_year}",
-        "#{current_year + 1}",
-        "#{current_year + 2}"
-      ])
+    assigns =
+      [
+        country: country,
+        federal_state: federal_state,
+        holiday_or_vacation_type: holiday_or_vacation_type
+      ] ++
+        CH.list_period_data(location_ids, today)
 
-    holiday_or_vacation_type =
-      Calendars.get_holiday_or_vacation_type_by_slug!(holiday_or_vacation_type_slug)
-
-    periods =
-      federal_state
-      |> Periods.list_current_and_future_periods(holiday_or_vacation_type)
-      |> check_future_periods(federal_state, holiday_or_vacation_type)
-
-    months = MehrSchulferien.Calendars.DateHelpers.get_months_map()
-
-    render(conn, "show_holiday_or_vacation_type.html",
-      country: country,
-      federal_state: federal_state,
-      holiday_or_vacation_type: holiday_or_vacation_type,
-      periods: periods,
-      months: months,
-      today: today,
-      current_year: current_year,
-      next_three_years: next_three_years
-    )
+    render(conn, "show_holiday_or_vacation_type.html", assigns)
   end
-
-  defp check_future_periods([], federal_state, holiday_or_vacation_type) do
-    case Periods.list_previous_periods(federal_state, holiday_or_vacation_type) do
-      [] -> raise MehrSchulferien.NoHolidayOrVacationTypePeriod
-      periods -> periods
-    end
-  end
-
-  defp check_future_periods(periods, _, _), do: periods
 
   def show(_conn, %{"modus" => _}) do
     raise MehrSchulferien.InvalidQueryParamsError
