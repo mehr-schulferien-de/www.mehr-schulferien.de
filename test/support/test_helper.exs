8c8c67fe6b453230ef33bbb7d23aa842409e911c
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-04-29 22:34:22 +0200

    This should resolve the "invalid session id" errors in your GitHub CI.

--- test/support/test_helper.exs
+++ test/support/test_helper.exs
@@ -4,3 +4,6 @@ Application.ensure_all_started(:wallaby)
 Application.ensure_all_started(:mehr_schulferien)
 
 Ecto.Adapters.SQL.Sandbox.mode(MehrSchulferien.Repo, :manual)
+
+# Explicitly start the Phoenix endpoint for Wallaby system tests
+{:ok, _} = MehrSchulferienWeb.Endpoint.start_link()
