99159de3a8f1b83bf6edba64756707cd7a5e9ee7
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-06-04 14:36:25 +0200

    Revert "Deployment bugfix"
    This reverts commit 97c77e24ff2c66d80bb71c4ea597889de3ec9a9e.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-import Config
+use Mix.Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -32,7 +32,7 @@ config :mehr_schulferien, MehrSchulferienWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-config :mehr_schulferien, MehrSchulferienWeb.Endpoint, server: true
+#     config :mehr_schulferien, MehrSchulferienWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
