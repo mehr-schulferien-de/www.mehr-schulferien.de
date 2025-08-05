d3023939623df98609e94e2b9936acd3769d1b09
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-08-05 11:06:42 +0200

    logging added

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -26,7 +26,8 @@ secret_key_base =
 config :mehr_schulferien, MehrSchulferienWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base,
-  debug_errors: true
+  debug_errors: true,
+  server: true
 
 # ## Using releases (Elixir v1.9+)
 #
