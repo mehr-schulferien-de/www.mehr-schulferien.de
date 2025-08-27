d2d61cbc8bb68cb84c9f843dbe7ddcf50f0e189f
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-08-27 19:30:52 +0200

    Bugfix

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     './js/**/*.js',
     '../lib/mehr_schulferien_web/**/*.*ex',
@@ -22,6 +23,12 @@ module.exports = {
     {
       pattern: /^border-(red|green|blue|yellow|orange|purple|pink|gray)-(100|200|300|400|500|600|700|800|900)$/
     },
+    // Dark mode variants need to be explicitly included
+    'dark:bg-gray-50', 'dark:bg-gray-100', 'dark:bg-gray-700', 'dark:bg-gray-800', 'dark:bg-gray-900',
+    'dark:text-gray-100', 'dark:text-gray-200', 'dark:text-gray-300', 'dark:text-gray-400',
+    'dark:border-gray-600', 'dark:border-gray-700', 'dark:border-gray-800',
+    'dark:bg-blue-500', 'dark:bg-blue-600', 'dark:bg-blue-700',
+    'dark:focus:ring-offset-gray-900',
     // Grid classes that might be generated
     {
       pattern: /^grid-cols-/
