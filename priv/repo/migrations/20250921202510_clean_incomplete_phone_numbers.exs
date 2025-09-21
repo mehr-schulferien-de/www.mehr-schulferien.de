a050089496be91f3fde1d4639706a0d6e470bc1e
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-09-21 22:33:33 +0200

    Use Ecto and not SQL

--- priv/repo/migrations/20250921202510_clean_incomplete_phone_numbers.exs
+++ priv/repo/migrations/20250921202510_clean_incomplete_phone_numbers.exs
@@ -1,21 +1,22 @@
 defmodule MehrSchulferien.Repo.Migrations.CleanIncompletePhoneNumbers do
   use Ecto.Migration
+  import Ecto.Query
 
   def up do
     # Update all entries where phone_number is exactly "+49 711 216"
     # Set both phone_number and fax_number to NULL for those addresses
-    execute """
-    UPDATE addresses
-    SET phone_number = NULL, fax_number = NULL
-    WHERE phone_number = '+49 711 216'
-    """
+    from(a in "addresses",
+      where: a.phone_number == "+49 711 216",
+      update: [set: [phone_number: nil, fax_number: nil]]
+    )
+    |> repo().update_all([])
 
     # Update remaining entries where fax_number is exactly "+49 711 216"
-    execute """
-    UPDATE addresses
-    SET fax_number = NULL
-    WHERE fax_number = '+49 711 216'
-    """
+    from(a in "addresses",
+      where: a.fax_number == "+49 711 216",
+      update: [set: [fax_number: nil]]
+    )
+    |> repo().update_all([])
   end
 
   def down do
