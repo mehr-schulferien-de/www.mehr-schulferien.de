e3d519af09a4383c1dd3f32c7ff2c051ecedffa5
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2026-01-14 13:03:03 +0100

    Move Gefahrenzone box higher on wiki school edit page
    - Repositioned delete section directly below "Schule nicht gefunden?"
    - Bump version to 4.23.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MehrSchulferien.MixProject do
   def project do
     [
       app: :mehr_schulferien,
-      version: "4.23.2",
+      version: "4.23.3",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
